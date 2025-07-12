4a037f81c6bf43dd41a044b0dc657a97fa7613f9
Adel Elsawaf <a.elsawaf@tamhub.com>
2025-07-12 17:37:34 +0300

    hot_fix(reorder procfile commands)

--- bin/start.sh
+++ bin/start.sh
@@ -6,9 +6,11 @@ php artisan migrate --force
 echo "🚀 Starting queue worker..."
 php artisan queue:work --tries=3 --timeout=90 >> storage/logs/worker.log 2>&1 &
 
+echo "Generating DOCS"
+php artisan scribe:generate
+
 echo "🌐 Starting web server..."
 php artisan serve --host=0.0.0.0 --port=${PORT:-8080}
 
-echo "Generating DOCS"
-php artisan scribe:generate
+
 
